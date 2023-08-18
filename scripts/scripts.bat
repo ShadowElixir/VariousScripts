96bfb81f439f71ea727bf60c16deea33c20621a6
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-08-18 22:28:21 +0300

    Update scripts.bat

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -5,7 +5,7 @@ echo Press 2 to Activate IDM (IDM Activation Script by lstprjct)
 echo Press 3 to Debloat Windows (winutil by Chris Titus Tech)
 echo Press 4 to Launch ShadowOS (ShadowOS by ShadowElixir)
 echo Press 5 to Launch noahOS (noahOS by NoahTheTechGenius)
-echo --------------------------------------------------------------
+echo ----------------------------------------------------------------
 set /p homeoption=
 if %homeoption% == 1 powershell "irm massgrave.dev/get | iex"
 if %homeoption% == 2 powershell "irm cutt.ly/idmactivation | iex"
