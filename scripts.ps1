cde43e2ebe5dab63c5e51056d57d64a0f8192498
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-08-18 18:45:20 +0300

    Add files via upload

---
+++ scripts.ps1
@@ -0,0 +1,20 @@
+# Got code from https://github.com/lstprjct/IDM-Activation-Script, Thank You lstprjct and credits to you
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/ShadowElixir/VariousScripts/main/scripts/scripts.bat'
+
+$FilePath = "$env:TEMP\scripts.bat"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
