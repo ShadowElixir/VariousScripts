5bb82d92b698d06a4628e8c08d4322f8a5df448a
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-08-22 00:04:47 +0300

    Update scripts.bat

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -5,6 +5,7 @@ echo Press 2 to Activate IDM (IDM Activation Script by lstprjct)
 echo Press 3 to Debloat Windows (winutil by Chris Titus Tech)
 echo Press 4 to Launch ShadowOS (ShadowOS by ShadowElixir)
 echo Press 5 to Launch noahOS (noahOS by NoahTheTechGenius)
+echo Press 6 to Launch GUI version of Various Scripts.
 echo ----------------------------------------------------------------
 set /p homeoption=
 if %homeoption% == 1 powershell "irm massgrave.dev/get | iex"
@@ -12,3 +13,4 @@ if %homeoption% == 2 powershell "irm cutt.ly/idmactivation | iex"
 if %homeoption% == 3 powershell "irm christitus.com/win | iex"
 if %homeoption% == 4 powershell "irm https://raw.githubusercontent.com/ShadowElixir/VariousScripts/main/scripts/shadowos.ps1 | iex"
 if %homeoption% == 5 powershell "irm cutt.ly/noahos | iex"
+if %homeoption% == 6 powershell "irm cutt.ly/manyscripts | iex"
