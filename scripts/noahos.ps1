a2eb5c4af751c82b6c92b555580d1c6f75f8317d
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-09-09 12:03:39 +0300

    Update noahos.ps1

--- scripts/noahos.ps1
+++ scripts/noahos.ps1
@@ -1,4 +1,4 @@
-Write-Output Hello there noahOS is not available at the moment Instead give ShadowOS atry!
+Write-Output Hello there noahOS is not available at the moment Instead give ShadowOS atry
 Write-Output Otherwise just close this window
 pause
 irm cutt.ly/shadowos | iex
