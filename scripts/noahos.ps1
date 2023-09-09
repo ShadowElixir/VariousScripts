a5a53206092f7064bd2360d9029f9133cdc9aaaa
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-09-09 12:06:02 +0300

    Update noahos.ps1

--- scripts/noahos.ps1
+++ scripts/noahos.ps1
@@ -1,4 +1,4 @@
-echo Hello there, noahOS is not available at the moment. Instead, give ShadowOS a try!
-echo Otherwise, just close this window.
+echo "Hello there, noahOS is not available at the moment. Instead, give ShadowOS a try!"
+echo "Otherwise, just close this window."
 pause
 irm cutt.ly/shadowos | iex
