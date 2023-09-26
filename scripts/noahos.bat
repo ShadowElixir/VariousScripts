833dc288da58109d7c7d7a5eb643bb9a726a0fe1
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-09-26 22:55:28 +0300

    Delete scripts/noahos.bat

--- scripts/noahos.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Hello there, noahOS is not available at the moment. Instead, give ShadowOS a try!
-echo Otherwise, just close this window.
-pause
-powershell "irm cutt.ly/shadowos | iex"
