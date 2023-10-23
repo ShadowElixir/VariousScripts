bea83a250a1f92ab11950a6ac2102233c4a1ba93
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-10-23 16:23:06 +0300

    Update and rename sweepy.ps1 to applauncher.ps1

--- scripts/applauncher.ps1
+++ scripts/applauncher.ps1
@@ -1,11 +1,11 @@
 # Got code from https://github.com/lstprjct/IDM-Activation-Script, Thank You lstprjct and credits to you
-# Sweepy by TrisoSoft
+# AppLauncher by Morpies
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/TrisoSoft/Sweepy/main/assistant.bat'
+$DownloadURL = 'https://raw.githubusercontent.com/Morpies/AppLauncher/main/app.bat'
 
-$FilePath = "$env:TEMP\assistant.bat"
+$FilePath = "$env:TEMP\app.bat"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
