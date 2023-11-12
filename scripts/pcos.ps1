c0b5dfbb8ce9491191638d72b159d7a587ce2a95
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-11-12 22:58:35 +0200

    Update and rename applauncher.ps1 to pcos.ps1

--- scripts/pcos.ps1
+++ scripts/pcos.ps1
@@ -3,9 +3,9 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/Boopki/AppLauncher/main/applauncher.bat'
+$DownloadURL = 'https://raw.githubusercontent.com/MooseSoftware/pcOS/main/pcos.bat'
 
-$FilePath = "$env:TEMP\applauncher.bat"
+$FilePath = "$env:TEMP\pcos.bat"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
