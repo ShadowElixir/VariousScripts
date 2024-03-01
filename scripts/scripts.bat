d176339dea144ed9d57c399ea468c27a18a50e5c
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2024-03-01 12:06:28 +0200

    Update scripts.bat

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -2,7 +2,7 @@
 title Various Scripts
 echo Various Scripts by ShadowElixir
 echo Press 1 to Activate Windows (MAS By MassGrave)
-echo Press 2 to Activate IDM (IDM Activation Script by lstprjct)
+echo Press 2 to Activate IDM (IDM Activation Script by WindowsAddict)
 echo Press 3 to Debloat Windows (winutil by Chris Titus Tech)
 echo Press 4 to Launch ShadowOS (ShadowOS by ShadowElixir)
 echo Press 5 to Launch Just Guitars
@@ -10,7 +10,7 @@ echo Press 6 to Launch GUI version of Various Scripts.
 echo ----------------------------------------------------------------
 set /p homeoption=
 if %homeoption% == 1 powershell "irm massgrave.dev/get | iex"
-if %homeoption% == 2 powershell "irm cutt.ly/idmactivation | iex"
+if %homeoption% == 2 powershell "irm https://massgrave.dev/ias | iex"
 if %homeoption% == 3 powershell "irm christitus.com/win | iex"
 if %homeoption% == 4 powershell "irm cutt.ly/shadowos | iex"
 if %homeoption% == 5 start "" https://cutt.ly/justguitars
