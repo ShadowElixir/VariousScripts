ad02ea418e6e27c90f1037c1d299c562965793f2
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2024-05-04 17:06:39 +0300

    Update scripts.bat

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -1,17 +1,18 @@
 @echo off
 title Various Scripts
 echo Various Scripts by ShadowElixir
-echo Press 1 to Activate Windows (MAS By MassGrave)
-echo Press 2 to Activate IDM (IDM Activation Script by WindowsAddict)
-echo Press 3 to Debloat Windows (winutil by Chris Titus Tech)
+echo Press 1 to Activate Windows and Office (MAS By MassGrave)
+echo Press 2 to Debloat Windows (winutil by Chris Titus Tech)
+echo Press 3 to Activate Better Pwsh Profile (Better Powershell Profile by ShadowElixir)
+echo                     *Requires Powershell Core 7
 echo Press 4 to Launch ShadowOS (ShadowOS by ShadowElixir)
 echo Press 5 to Launch Just Guitars
 echo Press 6 to Launch GUI version of Various Scripts.
-echo ----------------------------------------------------------------
+echo ------------------------------------------------------------------------------------
 set /p homeoption=
 if %homeoption% == 1 powershell "irm massgrave.dev/get | iex"
-if %homeoption% == 2 powershell "irm https://massgrave.dev/ias | iex"
-if %homeoption% == 3 powershell "irm christitus.com/win | iex"
+if %homeoption% == 2 powershell "irm christitus.com/win | iex"
+if %homeoption% == 3 powershell "irm cutt.ly/powerprofile | iex"
 if %homeoption% == 4 powershell "irm cutt.ly/shadowos | iex"
 if %homeoption% == 5 start "" https://cutt.ly/justguitars
 if %homeoption% == 6 powershell "irm cutt.ly/manyscripts | iex"
