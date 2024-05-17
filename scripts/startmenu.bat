fc9c659d09b09134b017d91d498924ba70e62a0f
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2024-05-17 21:15:08 +0300

    Update startmenu.bat

--- scripts/startmenu.bat
+++ scripts/startmenu.bat
@@ -1,30 +1,14 @@
 @echo off
 title Various Scripts
 :start
-echo Press 1 to use Windows Powershell 5.1. (default; recommended)
-echo Press 2 to use Powershell Core.
-echo ------------------------------------------------------------------------------------
-set /p pwsh=
-if %pwsh% == 1 goto pwsh5
-if %pwsh% == 2 goto pwshc
-
-:pwsh5
 echo Press 1 to use GUI version of VariousScripts. (recommended)
 echo Press 2 to use CMD version of VariousScripts.
 echo ------------------------------------------------------------------------------------
 set /p vs5=
-if %vs5% == 1 goto gui5
-if %vs5% == 2 goto cmd5
+if %vs% == 1 goto gui
+if %vs% == 2 goto cmd
 
-:pwshc
-echo Press 1 to use GUI version of VariousScripts. (recommended)
-echo Press 2 to use CMD version of VariousScripts.
-echo ------------------------------------------------------------------------------------
-set /p vsc=
-if %vsc% == 1 goto guic
-if %vsc% == 2 goto cmdc
-
-:gui5
+:gui
 echo Adding VariousScripts...
 echo @echo off >> "C:\VariousScripts\#ManyScripts.bat"
 echo powershell "irm cutt.ly/manyscripts | iex" >> "C:\VariousScripts\#ManyScripts.bat"
@@ -33,28 +17,10 @@ echo Done.
 pause
 exit
 
-:cmd5
+:cmd
 echo Adding VariousScripts...
 echo @echo off >> "C:\VariousScripts\#ManyScripts.bat"
 echo powershell "irm cutt.ly/cmdscripts | iex" >> "C:\VariousScripts\#ManyScripts.bat"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\#ManyScripts.lnk');$s.TargetPath='C:\\VariousScripts\\#ManyScripts.bat';$s.Save()"
-echo Done.
-pause
-exit
-
-:guic
-echo Adding VariousScripts...
-echo @echo off >> "C:\VariousScripts\#ManyScripts.bat"
-echo pwsh -Command "irm cutt.ly/manyscripts | iex" >> "C:\VariousScripts\#ManyScripts.bat"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\#ManyScripts.lnk');$s.TargetPath='C:\\VariousScripts\\#ManyScripts.bat';$s.Save()"
-echo Done.
-pause
-exit
-
-:cmdc
-echo Adding VariousScripts...
-echo @echo off >> "C:\VariousScripts\#ManyScripts.bat"
-echo pwsh -Command "irm cutt.ly/cmdscripts | iex" >> "C:\VariousScripts\#ManyScripts.bat"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\#ManyScripts.lnk');$s.TargetPath='C:\\VariousScripts\\#ManyScripts.bat';$s.Save()"
 echo Done.
 pause
