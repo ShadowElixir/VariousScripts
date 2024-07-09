4f9af8f06c3182c2aa41a5f18294078cefeda7b0
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2024-07-09 20:20:56 +0300

    Update scripts.bat

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -2,7 +2,7 @@
 title Various Scripts
 echo Various Scripts by ShadowElixir
 echo Press 1 to Activate Windows and Office (MAS By MassGrave)
-echo Press 2 to Debloat Windows (winutil by Chris Titus Tech)
+echo Press 2 to Debloat Windows (winutil by Chris Titus Tech; edited by me)
 echo Press 3 to Activate Better Pwsh Profile (by ShadowElixir)
 echo                     *Requires Powershell Core 7
 echo Press 4 to Launch ShadowOS (by ShadowElixir)
@@ -14,7 +14,7 @@ echo ---------------------------------------------------------------------------
 set /p homeoption=
 if %homeoption% == 0 powershell "irm cutt.ly/manystart | iex"
 if %homeoption% == 1 powershell "irm massgrave.dev/get | iex"
-if %homeoption% == 2 powershell "irm christitus.com/win | iex"
+if %homeoption% == 2 powershell "irm cutt.ly/debloat-windows | iex"
 if %homeoption% == 3 pwsh -Command "irm cutt.ly/powerprofile | iex"
 if %homeoption% == 4 powershell "irm cutt.ly/shadowos | iex"
 if %homeoption% == 5 start "" https://cutt.ly/justguitars
